c0a00ea3d5410cf6b4591d78738dcf3b8207a5fd
Hav1ck <156193188+Hav1ck@users.noreply.github.com>
2024-04-26 13:26:54 +0200

    new update checker and how strong is password check

---
+++ Encrypt/Encrypt/MainWindow.xaml.cs
@@ -0,0 +1,353 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace Encryption
+{
+    public partial class MainWindow : Window
+    {
+        EncryptionSystem encryption = new EncryptionSystem();
+        Account account = new Account();
+        UpdateChecker updateChecker = new UpdateChecker();
+        public MainWindow()
+        {
+            InitializeComponent();
+            UpdateChecker.Update();
+        }
+
+
+        private void SignInTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            // Set the visibility of UsernamePlaceholder TextBlock to Collapsed
+            SignInTextPlaceholder.Visibility = Visibility.Collapsed;
+        }
+
+        private void SignInPassword_GotFocus(object sender, RoutedEventArgs e)
+        {
+            // Set the visibility of PasswordPlaceholder TextBlock to Collapsed
+            SignInPasswordPlaceholder.Visibility = Visibility.Collapsed;
+        }
+        private void SignInTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            // Show the placeholder if the text box is empty
+            if (string.IsNullOrWhiteSpace(SignInTextBox.Text))
+            {
+                SignInTextPlaceholder.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void SignInPassword_LostFocus(object sender, RoutedEventArgs e)
+        {
+            // Show the placeholder if the password box is empty
+            if (string.IsNullOrWhiteSpace(SignInPasswordBox.Password))
+            {
+                SignInPasswordPlaceholder.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void RegisterTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            // Set the visibility of UsernamePlaceholder TextBlock to Collapsed
+            RegisterTextPlaceholder.Visibility = Visibility.Collapsed;
+        }
+
+        private void RegisterPassword_GotFocus(object sender, RoutedEventArgs e)
+        {
+            // Set the visibility of PasswordPlaceholder TextBlock to Collapsed
+            RegisterPasswordPlaceholder.Visibility = Visibility.Collapsed;
+        }
+        private void RegisterTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            // Show the placeholder if the text box is empty
+            if (string.IsNullOrWhiteSpace(RegisterTextBox.Text))
+            {
+                RegisterTextPlaceholder.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void RegisterPassword_LostFocus(object sender, RoutedEventArgs e)
+        {
+            // Show the placeholder if the password box is empty
+            if (string.IsNullOrWhiteSpace(RegisterPasswordBox.Password))
+            {
+                RegisterPasswordPlaceholder.Visibility = Visibility.Visible;
+            }
+        }
+        private void BackToMainMenu(object sender, RoutedEventArgs e)
+        {
+            RegisterTextBox.Text = "";
+            RegisterPasswordBox.Password = "";
+            SignInTextBox.Text = "";
+            SignInPasswordBox.Password = "";
+
+            RegisterHeader.Visibility = Visibility.Collapsed;
+            RegisterDescription.Visibility = Visibility.Collapsed;
+            RegisterTextBox.Visibility = Visibility.Collapsed;
+            RegisterTextPlaceholder.Visibility = Visibility.Collapsed;
+            RegisterPasswordBox.Visibility = Visibility.Collapsed;
+            RegisterPasswordPlaceholder.Visibility = Visibility.Collapsed;
+            RegisterButton.Visibility = Visibility.Collapsed;
+            RegisterAgreement.Visibility = Visibility.Collapsed;
+            RegisterAgreement2.Visibility = Visibility.Collapsed;
+            RegisterStartButton.Visibility = Visibility.Visible;
+            SignInStartButton.Visibility = Visibility.Visible;
+            RegisterTextBorder.Visibility = Visibility.Collapsed;
+            RegisterTextBorder2.Visibility = Visibility.Collapsed;
+            RegisterPasswordBorder.Visibility = Visibility.Collapsed;
+            RegisterPasswordBorder2.Visibility = Visibility.Collapsed;
+            CenterFrame.Visibility = Visibility.Collapsed;
+            SignInHeader.Visibility = Visibility.Collapsed;
+            SignInDescription.Visibility = Visibility.Collapsed;
+            SignInTextBox.Visibility = Visibility.Collapsed;
+            SignInTextPlaceholder.Visibility = Visibility.Collapsed;
+            SignInPasswordBox.Visibility = Visibility.Collapsed;
+            SignInPasswordPlaceholder.Visibility = Visibility.Collapsed;
+            SignInButton.Visibility = Visibility.Collapsed;
+            SignInAgreement.Visibility = Visibility.Collapsed;
+            SignInAgreement2.Visibility = Visibility.Collapsed;
+            SignInTextBorder.Visibility = Visibility.Collapsed;
+            SignInTextBorder2.Visibility = Visibility.Collapsed;
+            SignInPasswordBorder.Visibility = Visibility.Collapsed;
+            SignInPasswordBorder2.Visibility = Visibility.Collapsed;
+            BackToMainMenuButton.Visibility = Visibility.Collapsed;
+            SignInSuccess.Visibility = Visibility.Collapsed;
+            RegisterError.Visibility = Visibility.Collapsed;
+            MainBoxColor.Background = System.Windows.Media.Brushes.LightGray;
+        }
+
+        private void RegisterStartButton_Click(object sender, RoutedEventArgs e)
+        {
+            RegisterHeader.Visibility = Visibility.Visible;
+            RegisterDescription.Visibility = Visibility.Visible;
+            RegisterTextBox.Visibility = Visibility.Visible;
+            RegisterTextPlaceholder.Visibility = Visibility.Visible;
+            RegisterPasswordBox.Visibility = Visibility.Visible;
+            RegisterPasswordPlaceholder.Visibility = Visibility.Visible;
+            RegisterButton.Visibility = Visibility.Visible;
+            RegisterAgreement.Visibility = Visibility.Visible;
+            RegisterAgreement2.Visibility = Visibility.Visible;
+            RegisterStartButton.Visibility = Visibility.Collapsed;
+            SignInStartButton.Visibility = Visibility.Collapsed;
+            RegisterTextBorder.Visibility = Visibility.Visible;
+            RegisterTextBorder2.Visibility = Visibility.Visible;
+            RegisterPasswordBorder.Visibility = Visibility.Visible;
+            RegisterPasswordBorder2.Visibility = Visibility.Visible;
+            CenterFrame.Visibility = Visibility.Visible;
+            BackToMainMenuButton.Visibility = Visibility.Visible;
+            MainBoxColor.Background = System.Windows.Media.Brushes.DarkGray;
+        }
+        private void SignInStartButton_Click(object sender, RoutedEventArgs e)
+        {
+            SignInHeader.Visibility = Visibility.Visible;
+            SignInDescription.Visibility = Visibility.Visible;
+            SignInTextBox.Visibility = Visibility.Visible;
+            SignInTextPlaceholder.Visibility = Visibility.Visible;
+            SignInPasswordBox.Visibility = Visibility.Visible;
+            SignInPasswordPlaceholder.Visibility = Visibility.Visible;
+            SignInButton.Visibility = Visibility.Visible;
+            SignInAgreement.Visibility = Visibility.Visible;
+            SignInAgreement2.Visibility = Visibility.Visible;
+            RegisterStartButton.Visibility = Visibility.Collapsed;
+            SignInStartButton.Visibility = Visibility.Collapsed;
+            SignInTextBorder.Visibility = Visibility.Visible;
+            SignInTextBorder2.Visibility = Visibility.Visible;
+            SignInPasswordBorder.Visibility = Visibility.Visible;
+            SignInPasswordBorder2.Visibility = Visibility.Visible;
+            CenterFrame.Visibility = Visibility.Visible;
+            BackToMainMenuButton.Visibility = Visibility.Visible;
+            MainBoxColor.Background = System.Windows.Media.Brushes.DarkGray;
+        }
+        private void SignInButton_Click(object sender, RoutedEventArgs e)
+        {
+            string username = SignInTextBox.Text;
+            string password = SignInPasswordBox.Password;
+            login(username, password);
+        }
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+            string saltValue = encryption.salt();
+            string username = RegisterTextBox.Text;
+            string password = RegisterPasswordBox.Password;
+            register(saltValue, username, password);
+        }
+
+        // Method to register a new user
+        public void login(string username, string password)
+        {
+            try
+            {
+                // Read JSON file content
+                string jsonData = File.ReadAllText(FilePath);
+
+                // Deserialize JSON data into a list of Account objects
+                List<Account> accounts = JsonSerializer.Deserialize<List<Account>>(jsonData);
+
+                // Find the account with the provided username
+                Account userAccount = accounts.Find(account => account.Name == username);
+
+                if (userAccount != null)
+                {
+                    // Extract salt, random numbers, and final encrypted password
+                    string saltValue = userAccount.Salt;
+                    int[] randomNumbers = userAccount.RandomNumbers;
+                    string finalEncryptedPassword = userAccount.EncryptedPassword;
+
+                    // Append salt to the entered password
+                    password += saltValue;
+
+                    // Encrypt the entered password using the same process as during registration
+                    string encryptedPassword = encryption.EncryptPassword(password, randomNumbers);
+                    encryptedPassword = encryption.dqcr(encryptedPassword);
+                    encryptedPassword = encryption.dqcr2(encryptedPassword);
+
+                    // Check if the encrypted password matches the stored encrypted password
+                    if (encryptedPassword == finalEncryptedPassword)
+                    {
+                        // UI changes for successful login
+                        SignInSuccess.Visibility = Visibility.Visible;
+                        SignInHeader.Visibility = Visibility.Collapsed;
+                        SignInDescription.Visibility = Visibility.Collapsed;
+                        SignInTextBox.Visibility = Visibility.Collapsed;
+                        SignInTextPlaceholder.Visibility = Visibility.Collapsed;
+                        SignInPasswordBox.Visibility = Visibility.Collapsed;
+                        SignInPasswordPlaceholder.Visibility = Visibility.Collapsed;
+                        SignInButton.Visibility = Visibility.Collapsed;
+                        SignInAgreement.Visibility = Visibility.Collapsed;
+                        SignInAgreement2.Visibility = Visibility.Collapsed;
+                        SignInTextBorder.Visibility = Visibility.Collapsed;
+                        SignInTextBorder2.Visibility = Visibility.Collapsed;
+                        SignInPasswordBorder.Visibility = Visibility.Collapsed;
+                        SignInPasswordBorder2.Visibility = Visibility.Collapsed;
+                        CenterFrame.Visibility = Visibility.Collapsed;
+                        RegisterError.Visibility = Visibility.Collapsed;
+                    }
+                    else
+                    {
+                        // UI changes for incorrect password
+                        RegisterError.Visibility = Visibility.Visible;
+                        RegisterError.Foreground = System.Windows.Media.Brushes.Red;
+                        RegisterError.Text = "Password incorrect";
+                    }
+                }
+                else
+                {
+                    // UI changes for username not found
+                    RegisterError.Visibility = Visibility.Visible;
+                    RegisterError.Foreground = System.Windows.Media.Brushes.Red;
+                    RegisterError.Text = "Username not found";
+                }
+            }
+            catch (Exception ex)
+            {
+                // UI changes for error
+                RegisterError.Visibility = Visibility.Visible;
+                RegisterError.Foreground = System.Windows.Media.Brushes.Red;
+                RegisterError.Text = "An error occurred";
+            }
+        }
+        public void register(string saltValue, string username, string password)
+        {
+            try
+            {
+                // Read existing JSON data from file
+                List<Account> accounts = new List<Account>();
+                if (File.Exists(FilePath))
+                {
+                    string jsonData = File.ReadAllText(FilePath);
+                    accounts = JsonSerializer.Deserialize<List<Account>>(jsonData);
+                }
+
+                // Check if the username already exists
+                if (accounts.Exists(acc => acc.Name == username))
+                {
+                    // Username already exists, show error message
+                    RegisterError.Visibility = Visibility.Visible;
+                    RegisterError.Foreground = System.Windows.Media.Brushes.Red;
+                    RegisterError.Text = "This username is already taken";
+                    return;
+                }
+                if (username.Length < 4)
+                {
+                    // Password already exists, show error message
+                    RegisterError.Visibility = Visibility.Visible;
+                    RegisterError.Foreground = System.Windows.Media.Brushes.Red;
+                    RegisterError.Text = "This username is too short";
+                    return;
+                }
+                if (password.Length < 8)
+                {
+                    // Password already exists, show error message
+                    RegisterError.Visibility = Visibility.Visible;
+                    RegisterError.Foreground = System.Windows.Media.Brushes.Red;
+                    RegisterError.Text = "This password is too short";
+                    return;
+                }
+                // Check if the password contains at least one capital letter and one number
+                if (!Regex.IsMatch(password, "[A-Z]"))
+                {
+                    // Password does not meet complexity requirements, show error message
+                    RegisterError.Visibility = Visibility.Visible;
+                    RegisterError.Foreground = System.Windows.Media.Brushes.Red;
+                    RegisterError.Text = "Password must contain at least one capital letter";
+                    return;
+                }
+                if (!Regex.IsMatch(password, "[0-9]"))
+                {
+                    // Password does not meet complexity requirements, show error message
+                    RegisterError.Visibility = Visibility.Visible;
+                    RegisterError.Foreground = System.Windows.Media.Brushes.Red;
+                    RegisterError.Text = "Password must contain at least one number";
+                    return;
+                }
+                // Append salt to the password
+                password += saltValue;
+
+                // Perform encryption using Caesar cipher
+                (string encryptedPassword, int[] randomNumbers) = encryption.caesar(password);
+                encryptedPassword = encryption.dqcr(encryptedPassword);
+                encryptedPassword = encryption.dqcr2(encryptedPassword);
+
+                // Create a new account object
+                Account newAccount = new Account
+                {
+                    Name = username,
+                    Salt = saltValue,
+                    RandomNumbers = randomNumbers,
+                    EncryptedPassword = encryptedPassword,
+                    Date = DateTime.Now
+                };
+
+                // Add the new account to the list
+                accounts.Add(newAccount);
+
+                // Serialize the updated account list to JSON
+                string json = JsonSerializer.Serialize(accounts);
+
+                // Write the JSON data back to the file
+                File.WriteAllText(FilePath, json);
+
+                // Show success message
+                RegisterError.Visibility = Visibility.Visible;
+                RegisterError.Text = "Registration successful. Details saved in Data.json";
+                RegisterError.Foreground = System.Windows.Media.Brushes.Green;
+            }
+            catch (Exception ex)
+            {
+                // Show error message
+                RegisterError.Visibility = Visibility.Visible;
+                RegisterError.Foreground = System.Windows.Media.Brushes.Red;
+                RegisterError.Text = "An error occurred: " + ex.Message;
+            }
+        }
+
+    string FilePath = "../../Data.json";
+    }
+
+
+}
---
+++ Encrypt/Encrypt/UpdateChecker.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace Encryption
+{
+    class UpdateChecker
+    {
+        public static void Update()
+        {
+            string currentVersion = "v1.0.1";
+            char firstChar = currentVersion[1];
+            char secondChar = currentVersion[3];
+            char thirdChar = currentVersion[5];
+
+            int firstInt = int.Parse(firstChar.ToString());
+            int secondInt = int.Parse(secondChar.ToString());
+            int thirdInt = int.Parse(thirdChar.ToString());
+
+            int firstIntModified = firstInt + 1;
+            int secondIntModified = secondInt + 1;
+            int thirdIntModified = thirdInt + 1;
+
+            string firstStrModified = firstIntModified.ToString();
+            string secondStrModified = secondIntModified.ToString();
+            string thirdStrModified = thirdIntModified.ToString();
+
+            string firstPossiblePatch = $"v{firstStrModified}.{secondChar}.{thirdChar}";
+            string secondPossiblePatch = $"v{firstChar}.{secondStrModified}.{thirdChar}";
+            string thirdPossiblePatch = $"v{firstChar}.{secondChar}.{thirdStrModified}";
+
+            LatestVersion(firstPossiblePatch);
+            LatestVersion(secondPossiblePatch);
+            LatestVersion(thirdPossiblePatch);
+        }
+
+        public static void LatestVersion(string tag)
+        {
+            try
+            {
+                // URL-encode the tag
+                string encodedTag = Uri.EscapeDataString(tag);
+
+                // Add a forward slash before the tag in the URL
+                string url = $"https://api.github.com/repos/Hav1ck/AuthenticationProgram/releases/tags/{encodedTag}";
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                request.Method = "GET";
+                request.UserAgent = "AuthenticationProgram"; // Replace "Your-App-Name" with an appropriate user-agent string
+
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                {
+                    if (response.StatusCode == HttpStatusCode.OK)
+                    {
+                        Console.WriteLine("You can download new version at: https://github.com/Hav1ck/AuthenticationProgram");
+                    }
+                    else if (response.StatusCode == HttpStatusCode.NotFound)
+                    {
+                        // Do nothing for now if the tag doesn't exist
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Failed to check latest release information. Response code: {response.StatusCode}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred while checking for updates: " + ex.Message);
+            }
+        }
+    }
+}
